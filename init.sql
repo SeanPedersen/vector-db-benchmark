78c831121ad0701cf300570ff13a169a4b22b0c2
Sean Pedersen <seanpedersen96@protonmail.com>
2025-10-10 13:22:27 +0200

    Add version

--- init.sql
+++ init.sql
@@ -1,5 +1,5 @@
 -- Enable extensions for all index types
 CREATE EXTENSION IF NOT EXISTS vectorscale CASCADE;
-CREATE EXTENSION IF NOT EXISTS vchord CASCADE;
+CREATE EXTENSION IF NOT EXISTS vchord CASCADE WITH VERSION '0.5.2';
 
 -- Table will be created by insert.py with appropriate dimensions
